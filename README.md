04decfbbdc71156dd07fd373106146882cfd8435
Shubham Nanche <73213552+shubhamnanche@users.noreply.github.com>
2024-12-22 22:33:45 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Mahābhāratam
+
+<details>
+
+<summary><a href="https://en.wikipedia.org/wiki/Mahabharata">Intro</a></summary>
+
+The **Mahābhāratam** is a timeless epic, a tapestry of human experience, and a profound reflection on the complexities of life. Spanning generations, it chronicles the tale of the Kuru dynasty, centered around the rivalry between two branches of a royal family—the noble **Pandavas** and the ambitious **Kauravas**—culminating in the cataclysmic **Kurukshetra war**.
+
+At its heart, the Mahābhāratam is more than a story of kings and kingdoms. It delves into the eternal struggles of duty and desire, justice and power, loyalty and betrayal. It questions the moral fabric of human existence through its intricate characters: the unwavering **Dharma** of Yudhishthira, the indomitable strength of Bhima, the unfaltering archery of Arjuna, the enigmatic wisdom of Krishna, and the tragic heroism of Karna.
+
+Interwoven with philosophical discourses, including the revered **Bhagavad Gītā**, the Mahābhāratam is a mirror to the soul, exploring profound themes of fate, free will, and the inexorable consequences of human actions. It paints a vivid picture of a world where dharma is tested at every turn and where the paths to righteousness are shadowed by personal ambition and societal norms.
+
+Ultimately, the Mahābhāratam is not just a story—it is a revelation, a guide, and a quest for meaning in the grand theater of life. It reminds us that the pursuit of truth and the embrace of our human flaws are what make us eternal.
+
+</details>
---
+++ app/build.gradle.kts
@@ -0,0 +1,86 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+
+val keystoreProperties = Properties()
+keystoreProperties.load(FileInputStream(rootProject.file("keystore.properties")))
+
+android {
+    namespace = "com.ssk.mahabharatam"
+    compileSdk = 35
+
+    signingConfigs{
+        register("release"){
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
+    defaultConfig {
+        applicationId = "com.ssk.mahabharatam"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs["release"]
+        }
+        debug{
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+    buildFeatures {
+        buildConfig = true
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
