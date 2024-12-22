42375cbc4cb7953575b17a5eab01c43f1e7ddf69
Shubham Nanche <shubhamnanche19@gmail.com>
2024-12-22 22:33:45 +0530

    initial commit

---
+++ app/src/main/java/com/ssk/mahabharatam/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.ssk.mahabharatam
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ssk.mahabharatam.ui.theme.MahabharatamTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MahabharatamTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Surface(modifier = Modifier.padding(innerPadding)) {
+
+                    }
+                }
+            }
+        }
+    }
+}
